38d41f5cfbd315780e5e25e23ef86919a2a3f754
LJB <dev.leejb@gmail.com>
2021-05-11 13:54:07 +0000

    basic-brute_force-10973-prev_permutation

---
+++ Basic/Brute-force/10973-prev_permutation.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int permutation[10000];
+
+bool compare(int i, int j) {
+	return j < i;
+}
+ 
+void nextPermutation() {
+	int idx = -1;
+	
+	for (int i = N - 1; i > 0; --i) {
+		if (permutation[i] < permutation[i - 1]) {
+			idx = i;
+			
+			break;
+		}
+	}
+	
+	if (idx == -1) {
+		printf("-1");
+		
+		return;
+	}
+	
+	for (int i = N - 1; i >= idx; --i) {
+		if (permutation[idx - 1] > permutation[i]) {
+			int tmp = permutation[i];
+			permutation[i] = permutation[idx - 1];
+			permutation[idx - 1] = tmp;
+			
+			break;
+		}
+	}
+	
+	sort(&permutation[idx], &permutation[idx] + (N - idx), compare);
+	
+	for (int i = 0; i < N; ++i) {
+		printf("%d ", permutation[i]);
+	}
+}
+
+int main(void) {
+	scanf("%d", &N);
+	
+	for (int i = 0; i < N; ++i) {
+		scanf("%d", &permutation[i]);
+	}
+	
+	nextPermutation();
+	
+	return 0;
+}
