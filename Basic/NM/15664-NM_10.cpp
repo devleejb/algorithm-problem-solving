583bc4e2239b5dbab0f3fdeda1b2a37bf6d8b784
LJB <dev.leejb@gmail.com>
2021-05-14 13:25:16 +0000

    basic-NM-15664-NM_10

---
+++ Basic/NM/15664-NM_10.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int num[8], isUsed[8];
+vector<int> seq;
+
+void printAllSequence(int nowNum) {
+	int isUsedThisTurn[10000] = { 0 };
+	
+	if (seq.size() == M) {
+		for (int i = 0; i < M; ++i) {
+			printf("%d ", seq[i]);
+		}
+		
+		printf("\n");
+		
+		return;
+	}
+	
+	for (int i = nowNum; i < N; ++i) {
+		if (isUsed[i] == 0 && isUsedThisTurn[num[i]] == 0) {
+			isUsedThisTurn[num[i]] = 1;
+			isUsed[i] = 1;
+			seq.push_back(num[i]);
+			
+			printAllSequence(i);
+			
+			isUsed[i] = 0;
+			seq.pop_back();
+		}
+	}
+}
+
+int main(void) {
+	scanf("%d %d", &N, &M);
+	
+	for (int i = 0; i < N; ++i) {
+		scanf("%d", &num[i]);
+	}
+	
+	sort(num, num + N);
+
+	printAllSequence(0);
+	
+	return 0;
+}
