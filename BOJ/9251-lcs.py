b5a866abcc413eaab2cdfdd9ad56a2df1ed1f57a
devleejb <devleejb@gmail.com>
2024-05-13 21:58:57 +0900

    BOJ-9251-LCS

---
+++ BOJ/9251-lcs.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+input = stdin.readline
+
+A = input().strip()
+B = input().strip()
+
+dp = [[0] * (len(A) + 1) for _ in range(len(B) + 1)]
+
+for i in range(1, len(B) + 1):
+    for j in range(1, len(A) + 1):
+        if A[j - 1] == B[i - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len(B)][len(A)])
